f0cf74b7771bda8472414de6fd6603eed6a76dff
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-10-13 13:12:27 +0530

    Added Footer Section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,7 +6,7 @@ import Experience from "./components/experience/Experience";
 // import Portfolio from "./components/portfolio/Portfolio";
 // import Testimonials from "./components/testimonials/Testimonials";
 // import Contact from "./components/contact/Contact";
-// import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -17,8 +17,8 @@ const App = () => {
       <Experience />
       {/* <Portfolio />
       <Testimonials />
-      <Contact />
-      <Footer /> */}
+      <Contact />*/}
+      <Footer /> 
     </>
   );
 };
