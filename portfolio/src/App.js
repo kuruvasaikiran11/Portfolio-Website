f878fe8c04d7dd72dad9479e7b0dc3e16c2875b5
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-10-13 11:22:57 +0530

    Added About & Experience Section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,10 +3,10 @@ import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
-import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Testimonials from "./components/testimonials/Testimonials";
+// import Contact from "./components/contact/Contact";
+// import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -15,10 +15,10 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
-      <Portfolio />
+      {/* <Portfolio />
       <Testimonials />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
