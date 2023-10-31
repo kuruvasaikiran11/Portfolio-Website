b1e71f9519e96d33b69d74b619ea9e1367b94313
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-10-31 21:17:47 +0530

    Made adjustments

--- portfolio/src/components/about/About.js
+++ portfolio/src/components/about/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import profile_picture from "../../assets/about-img2.png";
+import profile_picture from "../../assets/about-img2.jpg";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
